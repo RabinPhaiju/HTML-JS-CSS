f3e31a6378a3ba3ea520b87b309f2f9065239649
RabinPhaiju <rabinphaiju15@gmail.com>
2021-02-09 09:33:27 +0545

    basic calculator added

---
+++ Caclculator/script.js
@@ -0,0 +1,145 @@
+var display = document.querySelector(".display-text");
+const nine = document.querySelector(".nine");
+const eight = document.querySelector(".eight");
+const seven = document.querySelector(".seven");
+const divide = document.querySelector(".divide");
+
+const six = document.querySelector(".six");
+const five = document.querySelector(".five");
+const four = document.querySelector(".four");
+const times = document.querySelector(".times");
+
+const three = document.querySelector(".three");
+const two = document.querySelector(".two");
+const one = document.querySelector(".one");
+const diff = document.querySelector(".diff");
+
+const clear = document.querySelector(".clear");
+const zero = document.querySelector(".zero");
+const sum = document.querySelector(".sum");
+const total = document.querySelector(".total");
+
+var number = display.innerHTML;
+
+function middle() {
+  if (number == 0) {
+    number = "";
+  } else if (number.length > 2) {
+    while (number.includes(" ")) {
+      number = number.replace(" ", "");
+    }
+    number = number.replace(/\d{3}(?=.)/g, "$& ");
+  }
+  if (number.length > 16) {
+    display.style.fontSize = "18px";
+  }
+}
+total.addEventListener("click", () => {
+  if (number == "") {
+  } else if (!["-", "+", "/", "*"].includes(number[number.length - 1])) {
+    while (number.includes(" ")) {
+      number = number.replace(" ", "");
+    }
+    number = eval(number);
+
+    display.innerHTML = number;
+  }
+});
+
+nine.addEventListener("click", () => {
+  middle();
+  number = number + "9";
+  display.innerHTML = number;
+});
+eight.addEventListener("click", () => {
+  middle();
+  number = number + "8";
+  display.innerHTML = number;
+});
+seven.addEventListener("click", () => {
+  middle();
+  number = number + "7";
+  display.innerHTML = number;
+});
+six.addEventListener("click", () => {
+  middle();
+  number = number + "6";
+  display.innerHTML = number;
+});
+five.addEventListener("click", () => {
+  middle();
+  number = number + "5";
+  display.innerHTML = number;
+});
+four.addEventListener("click", () => {
+  middle();
+  number = number + "4";
+  display.innerHTML = number;
+});
+three.addEventListener("click", () => {
+  middle();
+  number = number + "3";
+  display.innerHTML = number;
+});
+two.addEventListener("click", () => {
+  middle();
+  number = number + "2";
+  display.innerHTML = number;
+});
+one.addEventListener("click", () => {
+  middle();
+  number = number + "1";
+  display.innerHTML = number;
+});
+
+function signCheck() {
+  if (
+    number[number.length - 1] != "/" &&
+    number[number.length - 1] != "*" &&
+    number[number.length - 1] != "+" &&
+    number[number.length - 1] != "-"
+  ) {
+    return "true";
+  }
+}
+
+divide.addEventListener("click", () => {
+  middle();
+  if (signCheck() == "true") {
+    number = number + "/";
+  }
+
+  display.innerHTML = number;
+});
+times.addEventListener("click", () => {
+  middle();
+  if (signCheck() == "true") {
+    number = number + "*";
+  }
+  display.innerHTML = number;
+});
+diff.addEventListener("click", () => {
+  middle();
+  if (signCheck() == "true") {
+    number = number + "-";
+  }
+  display.innerHTML = number;
+});
+sum.addEventListener("click", () => {
+  middle();
+  if (signCheck() == "true") {
+    number = number + "+";
+  }
+  display.innerHTML = number;
+});
+
+clear.addEventListener("click", () => {
+  middle();
+  number = "0";
+  display.innerHTML = number;
+});
+zero.addEventListener("click", () => {
+  middle();
+  number = number + "0";
+  display.innerHTML = number;
+});
--- README.md
+++ README.md
@@ -3,4 +3,5 @@
 1. [Portfolio](https://rabinphaiju.github.io/HTML-JS-CSS/Porfolio/)
 2. [UI 1](https://rabinphaiju.github.io/HTML-JS-CSS/UI%201/)
 3. [UI 2](https://rabinphaiju.github.io/HTML-JS-CSS/UI%202/)
-3. [Shopping Cart](https://rabinphaiju.github.io/HTML-JS-CSS/Shopping%20Cart%201/)
+4. [Shopping Cart](https://rabinphaiju.github.io/HTML-JS-CSS/Shopping%20Cart%201/)
+5. [Basic Calculator](https://rabinphaiju.github.io/HTML-JS-CSS/Calculator/)
